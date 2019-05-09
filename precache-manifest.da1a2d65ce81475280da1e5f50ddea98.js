990fb221bed4fa6f7040f926ac831e02c0177c09
Alex Kolarski <aleks.rk@gmail.com>
2019-05-10 02:56:22 +0300

    Fixes

---
+++ precache-manifest.da1a2d65ce81475280da1e5f50ddea98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29577e9aaecc4bd752160cf3295c49d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b01af83a80ba697a8dc",
+    "url": "/static/css/2.28c6ddb0.chunk.css"
+  },
+  {
+    "revision": "df448a3b44b86b01b590",
+    "url": "/static/css/main.46ebc776.chunk.css"
+  },
+  {
+    "revision": "6b01af83a80ba697a8dc",
+    "url": "/static/js/2.1cb5e317.chunk.js"
+  },
+  {
+    "revision": "df448a3b44b86b01b590",
+    "url": "/static/js/main.49e775d2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0435ee98d6bfa860c479caa657a170c5",
+    "url": "/static/media/logo3.0435ee98.png"
+  }
+]);
