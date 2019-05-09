7ff9eeab19e700985142450525b56a00815a449e
Alex Kolarski <aleks.rk@gmail.com>
2019-05-09 23:54:47 +0300

    React JS version

---
+++ precache-manifest.6e2a7e5eec15a4b8282ed20ee95e7e25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5061a0f93981fd163f3fde2c0e5bd370",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff5a4c13ebe8c160d657",
+    "url": "/static/css/2.28c6ddb0.chunk.css"
+  },
+  {
+    "revision": "e985858ff1d08de5ae8d",
+    "url": "/static/css/main.77613cd5.chunk.css"
+  },
+  {
+    "revision": "ff5a4c13ebe8c160d657",
+    "url": "/static/js/2.f9a2f858.chunk.js"
+  },
+  {
+    "revision": "e985858ff1d08de5ae8d",
+    "url": "/static/js/main.24f34fe7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0435ee98d6bfa860c479caa657a170c5",
+    "url": "/static/media/logo3.0435ee98.png"
+  }
+]);
