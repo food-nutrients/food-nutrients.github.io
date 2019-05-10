397dade1dbd557da38f214aed6efad99d156d1c0
Alex Kolarski <aleks.rk@gmail.com>
2019-05-10 08:33:49 +0300

    All mobile files

---
+++ precache-manifest.a347e6d7d3b74efc8591133ec51f429b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3adb8e7b3c92d9634d351bbf3cb8f1ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "971523a940ce9a25121a",
+    "url": "/static/css/2.28c6ddb0.chunk.css"
+  },
+  {
+    "revision": "a5884ba80728559f291a",
+    "url": "/static/css/main.7a75f683.chunk.css"
+  },
+  {
+    "revision": "971523a940ce9a25121a",
+    "url": "/static/js/2.99c56cab.chunk.js"
+  },
+  {
+    "revision": "a5884ba80728559f291a",
+    "url": "/static/js/main.1fcaa9ce.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0435ee98d6bfa860c479caa657a170c5",
+    "url": "/static/media/logo3.0435ee98.png"
+  }
+]);
